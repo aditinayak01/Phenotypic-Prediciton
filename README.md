e45900972efd5a8211b9d0c56f208bfd73130e86
Aditi Nayak <adnayak@cs.stonybrook.edu>
2018-04-01 10:57:30 -0400

    New repos added

---
+++ README.md
@@ -0,0 +1,3 @@
+# phenotypic_prediction
+Developed a model on label prediction using feature selection and dimension reduction algorithm which beats the Salmon baseline for population types coming from different phenotypes
+• Technologies Used: Python
---
+++ make_csv.py
@@ -0,0 +1,24 @@
+import os
+import pandas as pd
+from os import listdir
+
+# create csv files from quant.sf files, keep only Name and TPM columns
+def make_csv_files(raw_path_string, csv_path, slash):
+    print "Starting creating of csv files"
+
+    # if directory for storing csv files does not exist, create it
+    if not os.path.exists(csv_path):
+        os.makedirs(csv_path)
+    elif os.path.exists(csv_path) and len(listdir(csv_path)) > 0:
+        print "Some csv files already present. Not creating again."
+        return
+
+    # create csv files
+    columns = ['Name', 'TPM']
+    input_files = listdir(raw_path_string)
+    for file in input_files:
+        data = pd.read_csv(raw_path_string + file + slash + 'bias' + slash + 'quant.sf', sep='\t', header=0, dtype='unicode')
+        data = data[columns]
+        data.to_csv(csv_path + slash + file + '.csv', header = columns, index=False)                
+        
+    print "Created csv files"
